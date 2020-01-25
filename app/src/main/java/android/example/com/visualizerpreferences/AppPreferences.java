4b15110166c64daab383aa2479918a580f78ffd5
Jacob <jaureguijacob57@gmail.com>
2020-01-25 16:55:16 -0700

    initial commit

---
+++ app/src/main/java/android/example/com/visualizerpreferences/AppPreferences.java
@@ -0,0 +1,16 @@
+package android.example.com.visualizerpreferences;
+
+public class AppPreferences {
+    public static final String COLOR = "color";
+    //COLOR setting values
+    public static final String RED = "red";
+    public static final String BLUE = "blue";
+    public static final String GREEN = "green";
+
+    //visual components settings
+    public static final String SHOW_TREBLE = "treble";
+    public static final String SHOW_BASS = "bass";
+    public static final String SHOW_MID = "mid";
+
+    //
+}
